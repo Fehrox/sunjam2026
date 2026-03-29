27326c8acf46398123835e5f68993fc6556075d9
Christian Hodges <joseph.c.hodges@gmail.com>
2026-03-29 15:06:21 +1000

    Created a win game actor working

--- Source/TrainGame/Private/World/TrainWinActor.cpp
+++ Source/TrainGame/Private/World/TrainWinActor.cpp
@@ -27,8 +27,9 @@ void ATrainWinActor::BeginPlay()
 
 void ATrainWinActor::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && OtherActor->IsA(ATrain::StaticClass()))
+	if (!bHasTriggered && OtherActor && OtherActor->IsA(ATrain::StaticClass()))
 	{
+		bHasTriggered = true;
 		OnTrainWin.Broadcast();
 	}
 }
--- Source/TrainGame/Public/World/TrainWinActor.h
+++ Source/TrainGame/Public/World/TrainWinActor.h
@@ -31,4 +31,7 @@ protected:
 
 	UFUNCTION()
 	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	bool bHasTriggered = false;
 };
